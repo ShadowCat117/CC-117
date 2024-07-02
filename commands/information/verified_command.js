625b875497aa0288284abd2d21d8d712fae338f8
Jamie <JamCal117@gmail.com>
2024-07-02 18:28:25 +0100

    Update verified

---
+++ commands/information/verified_command.js
@@ -0,0 +1,163 @@
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const verified = require('../../functions/verified');
+const messages = require('../../functions/messages');
+const PagedMessage = require('../../message_objects/PagedMessage');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('verified')
+        .setDescription('Check who in your server is verified.'),
+    ephemeral: false,
+    async execute(interaction) {
+        const loadingEmbed = new EmbedBuilder()
+            .setDescription('Finding verified members')
+            .setColor(0x00ff00);
+
+        const message = await interaction.editReply({ embeds: [loadingEmbed] });
+
+        const guildId = interaction.guild.id;
+        const filePath = path.join(__dirname, '..', '..', 'configs', `${guildId}.json`);
+
+        try {
+            let config = {};
+
+            if (fs.existsSync(filePath)) {
+                const fileData = fs.readFileSync(filePath, 'utf-8');
+                config = JSON.parse(fileData);
+            } else {
+                await interaction.editReply({ 
+                    content: 'You do not have a guild set. Use /setguild to set one.',
+                    embeds: [], 
+                });
+            }
+
+            const guildName = config.guildName;
+
+            if (!guildName || guildName === '') {
+                await interaction.editReply({ 
+                    content: 'You do not have a guild set. Use /setguild to set one.',
+                    embeds: [], 
+                });
+            }
+
+            // Call verified
+            const response = await verified(guildName, interaction);
+
+            const embeds = [];
+            const row = new ActionRowBuilder();
+
+            if (response.guildName === '') {
+                const responseEmbed = new EmbedBuilder();
+
+                // Unknown guild
+                responseEmbed
+                    .setTitle('Guild does not exist')
+                    .setDescription(`Unable to find ${guildName}, it may have been deleted or the API is down.`)
+                    .setColor(0xff0000);
+
+                embeds.push(responseEmbed);
+            } else {
+                // Found guild, if more than 45 players we need to make pages for the embed, otherwise 1 page will work
+                if (response.verifiedMembers.length > 45) {
+                    const pages = [];
+                    for (let i = 0; i < response.verifiedMembers.length; i += 45) {
+                        pages.push(response.verifiedMembers.slice(i, i + 45));
+                    }
+
+                    for (const page of pages) {
+                        const responseEmbed = new EmbedBuilder();
+                        responseEmbed
+                            .setTitle(`[${response.guildPrefix}] ${response.guildName} Verified Members`)
+                            .setColor(0x00ffff);
+                    
+                        let usernameValue = '';
+                        let verifiedValue = '';
+                    
+                        for (const player of page) {
+                            usernameValue += player.username.replaceAll('_', '\\_') + '\n';
+                    
+                            if (player.verifiedMember) {
+                                verifiedValue += `<@${player.verifiedMember}>\n`;
+                            } else {
+                                verifiedValue += 'Not verified\n';
+                            }
+                        }
+                    
+                        responseEmbed
+                            .addFields(
+                                { name: 'Username', value: usernameValue, inline: true },
+                                { name: 'Discord User', value: verifiedValue, inline: true },
+                            );
+                    
+                        embeds.push(responseEmbed);
+                    }
+
+                    messages.addMessage(message.id, new PagedMessage(message, embeds));
+
+                    const previousPage = new ButtonBuilder()
+                        .setCustomId('previous')
+                        .setStyle(ButtonStyle.Primary)
+                        .setEmoji('⬅️');
+
+                    const nextPage = new ButtonBuilder()
+                        .setCustomId('next')
+                        .setStyle(ButtonStyle.Primary)
+                        .setEmoji('➡️');
+
+                    row.addComponents(previousPage, nextPage);
+                } else {
+                    const responseEmbed = new EmbedBuilder();
+
+                    responseEmbed
+                        .setTitle(`[${response.guildPrefix}] ${response.guildName} Verified Members`)
+                        .setColor(0x00ffff);
+
+                    if (response.verifiedMembers.length > 0) {
+                        let usernameValue = '';
+                        let verifiedValue = '';
+
+                        for (const player of response.verifiedMembers) {
+                            usernameValue += player.username.replaceAll('_', '\\_') + '\n';
+                    
+                            if (player.verifiedMember) {
+                                verifiedValue += `${player.verifiedMember}\n`;
+                            } else {
+                                verifiedValue += 'Not verified\n';
+                            }
+                        }
+
+                        responseEmbed
+                            .addFields(
+                                { name: 'Username', value: usernameValue, inline: true },
+                                { name: 'Discord User', value: verifiedValue, inline: true },
+                            );
+                    }
+
+                    embeds.push(responseEmbed);
+                }
+            }
+
+            if (row.components.length > 0) {
+                await interaction.editReply({ 
+                    embeds: [embeds[0]],
+                    components: [row],
+                });
+            } else {
+                await interaction.editReply({ embeds: [embeds[0]] });
+            }
+        } catch (error) {
+            await interaction.editReply({ 
+                content: 'Failed to lookup verified members.',
+                embeds: [], 
+            });
+        }
+    },
+};
--- functions/verified.js
+++ functions/verified.js
@@ -1,78 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('database/database.db');
+const axios = require('axios');
 const VerifiedMember = require('../message_objects/VerifiedMember');
 
-async function allAsync(query, params) {
-    return new Promise((resolve, reject) => {
-        db.all(query, params, function(err, rows) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
-}
-
-async function verified(interaction) {
-    const guildId = interaction.guild.id;
-    const directoryPath = path.join(__dirname, '..', 'configs');
-    const filePath = path.join(directoryPath, `${guildId}.json`);
-
-    try {
-        let config = {};
-
-        if (fs.existsSync(filePath)) {
-            const fileData = fs.readFileSync(filePath, 'utf-8');
-            config = JSON.parse(fileData);
-        }
-
-        const guildName = config.guildName;
-
-        // Return if no guild name set
-        if (!guildName) {
-        }
-
-        // Get all players in guild, sort a-z
-        const rows = await allAsync('SELECT username FROM players WHERE guildName = ? ORDER BY username DESC', [guildName]);
-
-        // No players in guild
-        if (rows.length === 0) {
-        }
-
-        // Create the list of verified members
-        const verifiedMembers = rows.map(row => {
-            return new VerifiedMember(row.username, interaction.guild.members.cache.values());
-        });
-
-        const pages = [];
-        let verifiedMembersPage = '```diff\n';
-        let counter = 0;
-
-        // Create the pages of verified members
-        // Show 20 per page
-        verifiedMembers.forEach((player) => {
-            if (counter === 20) {
-                verifiedMembersPage += '```';
-                pages.push(verifiedMembersPage);
-                verifiedMembersPage = '```diff\n' + player.toString();
-                counter = 1;
-            } else {
-                verifiedMembersPage += player.toString();
-                counter++;
-            }
-        });
-
-        // If last page didn't contain 20 members, finish it
-        if (counter !== 20) {
-            verifiedMembersPage += '```';
-            pages.push(verifiedMembersPage);
-        }
-    } catch (error) {
-        console.log(error);
+async function verified(guildName, interaction) {
+    const guildJson = (await axios.get(`https://api.wynncraft.com/v3/guild/${guildName}`)).data;
+    
+    // FIXME: Handle errors better
+    if (!guildJson || !guildJson.name) {
+        return ({ guildName: '', guildPrefix: '', verifiedMembers: [] });
     }
+
+    const verifiedMembers = [];
+
+    for (const rank in guildJson.members) {
+        if (rank === 'total') continue;
+
+        const rankMembers = guildJson.members[rank];
+
+        for (const member in rankMembers) {
+            // Need to parse the cache in here instead of parsing it into this function from the command as for some reason
+            // it doesn't work after the 1st VerifiedMember
+            verifiedMembers.push(new VerifiedMember(member, interaction.guild.members.cache.values()));
+        }
+    }
+
+    return ({ guildName: guildJson.name, guildPrefix: guildJson.prefix, verifiedMembers: verifiedMembers });
 }
 
 module.exports = verified;
