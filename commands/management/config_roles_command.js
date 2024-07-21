a0e9cfc02d88dfc6421e367ea5213f898ee02569
Jamie <JamCal117@gmail.com>
2024-07-21 10:58:12 +0100

    Update config_roles

---
+++ commands/management/config_roles_command.js
@@ -0,0 +1,211 @@
+const {
+    EmbedBuilder,
+    SlashCommandBuilder,
+} = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const createConfig = require('../../functions/create_config');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('config_roles')
+        .setDescription('Update role configurations')
+        .addStringOption((option) =>
+            option.setName('option')
+                .setDescription('The role option to update')
+                .setRequired(true)
+                .addChoices({
+                    name: 'Admin Role',
+                    value: 'adminRole',
+                }, {
+                    name: 'Owner Role',
+                    value: 'ownerRole',
+                }, {
+                    name: 'Chief Role',
+                    value: 'chiefRole',
+                }, {
+                    name: 'Strategist Role',
+                    value: 'strategistRole',
+                }, {
+                    name: 'Captain Role',
+                    value: 'captainRole',
+                }, {
+                    name: 'Recruiter Role',
+                    value: 'recruiterRole',
+                }, {
+                    name: 'Recruit Role',
+                    value: 'recruitRole',
+                }, {
+                    name: 'Ally Owner Role',
+                    value: 'allyOwnerRole',
+                }, {
+                    name: 'Ally Role',
+                    value: 'allyRole',
+                }, {
+                    name: 'Champion Role',
+                    value: 'championRole',
+                }, {
+                    name: 'Hero Role',
+                    value: 'heroRole',
+                }, {
+                    name: 'VIP+ Role',
+                    value: 'vipPlusRole',
+                }, {
+                    name: 'VIP Role',
+                    value: 'vipRole',
+                }, {
+                    name: 'Veteran Role',
+                    value: 'vetRole',
+                }, {
+                    name: 'Verified Role',
+                    value: 'verifiedRole',
+                }, {
+                    name: 'Unverified Role',
+                    value: 'unverifiedRole',
+                }, {
+                    name: 'Member of Guild Role',
+                    value: 'memberOfRole',
+                }, {
+                    name: 'Administrator Role',
+                    value: 'administratorRole',
+                }, {
+                    name: 'Moderator Role',
+                    value: 'moderatorRole',
+                }, {
+                    name: 'Content Team Role',
+                    value: 'contentTeamRole',
+                }, {
+                    name: 'Giveaway Role',
+                    value: 'giveawayRole',
+                }, {
+                    name: 'Events Role',
+                    value: 'eventsRole',
+                }))
+        .addRoleOption((option) =>
+            option.setName('role')
+                .setDescription('The role to set for the configuration option')
+                .setRequired(true),
+        ),
+    ephemeral: true,
+    async execute(interaction) {
+        const option = interaction.options.getString('option');
+        const role = interaction.options.getRole('role');
+
+        const loadingEmbed = new EmbedBuilder()
+            .setDescription(`Setting ${option} to ${role}`)
+            .setColor(0x00ff00);
+
+        await interaction.editReply({ embeds: [loadingEmbed] });
+
+        let config = {};
+
+        const responseEmbed = new EmbedBuilder();
+
+        try {
+            const guildId = interaction.guild.id;
+            const filePath = path.join(__dirname, '..', '..', 'configs', `${guildId}.json`);
+
+            if (fs.existsSync(filePath)) {
+                const fileData = fs.readFileSync(filePath, 'utf-8');
+                config = JSON.parse(fileData);
+            } else {
+                await createConfig(interaction.client, guildId);
+
+                const fileData = fs.readFileSync(filePath, 'utf-8');
+                config = JSON.parse(fileData);
+            }
+
+            const adminRoleId = config.adminRole;
+            const memberRoles = interaction.member.roles.cache;
+            const memberOfRole = config.memberOfRole;
+
+            // If the member of role is used, it is required
+            if (memberOfRole && (interaction.member.id !== interaction.member.guild.ownerId) && (!memberRoles.has(memberOfRole))) {
+                responseEmbed
+                    .setDescription('You do not have the required permissions to run this command.')
+                    .setColor(0xff0000);
+                await interaction.editReply({ embeds: [responseEmbed] });
+                return;
+            }
+
+            // Can only be ran by the owner or an admin
+            if ((interaction.member.id !== interaction.member.guild.ownerId) && (!memberRoles.has(adminRoleId) && interaction.member.roles.highest.position < interaction.guild.roles.cache.get(adminRoleId).position)) {
+                responseEmbed
+                    .setDescription('You do not have the required permissions to run this command.')
+                    .setColor(0xff0000);
+                await interaction.editReply({ embeds: [responseEmbed] });
+                return;
+            }
+        } catch (error) {
+            console.error(error);
+            responseEmbed
+                .setDescription('Error changing config.')
+                .setColor(0xff0000);
+            await interaction.editReply({ embeds: [responseEmbed] });
+            return;
+        }
+
+        const guildId = interaction.guild.id;
+        const filePath = path.join(__dirname, '..', '..', 'configs', `${guildId}.json`);
+
+        try {
+            const guild = interaction.guild;
+            const botRole = guild.roles.cache.find(roleSearch => roleSearch.managed && roleSearch.members.has(interaction.client.user.id));
+
+            let message;
+
+            // If the bot does not have permission to give the role, let the user know
+            if (botRole) {
+                if (role.comparePositionTo(botRole) > 0 && option !== 'adminRole') {
+                    // Admin role is not applied to anyone so it doesn't need permission to handle it
+                    message = `Configuration option ${option} updated successfully to ${role}.\n\nThe ${role} role is currently above the ${botRole} role in your hierarchy, this means that I will not be able to add that role to or remove that role from members, please change this so I can manage the role correctly!`;
+                } else {
+                    message = `Configuration option ${option} updated successfully to ${role}.`;
+                }
+            } else {
+                message = `Configuration option ${option} updated successfully to ${role}.\nFor some reason my role was not found on your server. Please try kicking and inviting me again to try and fix this. Your config options will be saved.`;
+            }
+
+            // Save the option to the config
+            switch (option) {
+                case 'adminRole':
+                case 'ownerRole':
+                case 'chiefRole':
+                case 'strategistRole':
+                case 'captainRole':
+                case 'recruiterRole':
+                case 'recruitRole':
+                case 'allyOwnerRole':
+                case 'allyRole':
+                case 'championRole':
+                case 'heroRole':
+                case 'vipPlusRole':
+                case 'vipRole':
+                case 'vetRole':
+                case 'verifiedRole':
+                case 'unverifiedRole':
+                case 'memberOfRole':
+                case 'administratorRole':
+                case 'moderatorRole':
+                case 'contentTeamRole':
+                case 'giveawayRole':
+                case 'eventsRole':
+                    config[option] = role.id;
+                    break;
+            }
+
+            fs.writeFileSync(filePath, JSON.stringify(config, null, 2), 'utf-8');
+
+            responseEmbed
+                .setDescription(`${message}`)
+                .setColor(0x00ffff);
+        } catch (error) {
+            console.error(`Error updating configuration option: ${error}`);
+            responseEmbed
+                .setDescription('An error occured whilst updating config file.')
+                .setColor(0xff0000);
+        }
+
+        await interaction.editReply({ embeds: [responseEmbed] });
+    },
+};
